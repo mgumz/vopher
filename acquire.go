mgumz/vopher

commit a4e7ef81d7ea7dc7839e61ed80846106e318702c

add support for .tar.*, .vba, .vba.gz etc
support acquire vim plugins in other formats than .zip. *vopher* is now
able to handle the following formats:

- .zip
- .tar.gz
- .tar.bzip2
- .tar.bz2
- .tar.xz
- .tar.lzma
- .vba ("vimball")
- .vba.gz

acquire.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"archive/zip"
 	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
@@ -16,61 +15,29 @@ import (
 // fetch 'url' and extract it into 'base'. skip 'skip_dirs'
 // leading directories in filenames in zip while extracting
 // the contents.
-func acquire(base, url string, skip_dirs int, checkSha1 string) error {
+func acquire(base, ext, url string, archive PluginArchive, skip_dirs int, checkSha1 string) error {
 
-	if err := os.MkdirAll(base, 0777); err != nil {
+	var (
+		name = base
+		err  = os.MkdirAll(name, 0777)
+	)
+
+	if err != nil {
 		return fmt.Errorf("mkdir %q: %s", base, err)
 	}
-
-	name := base + ".zip"
-	if err := httpget(name, url, checkSha1); err != nil {
+	if filepath.Ext(name) == "" {
+		name += ext
+	}
+	if err = httpget(name, url, checkSha1); err != nil {
 		return err
 	}
-	zfile, err := zip.OpenReader(name)
+	file, err := os.Open(name)
 	if err != nil {
 		return err
 	}
-	defer zfile.Close()
-	for _, f := range zfile.File {
-		idx := index_byte_n(f.Name, '/', skip_dirs)
+	defer file.Close()
 
-		oname := f.Name[idx+1:]
-
-		// root-directory
-		//   pname/      <- root-directory
-		//   pname/a.vim
-		if oname == "" {
-			continue
-		}
-
-		oname = filepath.Join(base, filepath.Clean(oname))
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(oname, 0777)
-			continue
-		}
-
-		// TODO: call only if needed
-		os.MkdirAll(filepath.Dir(oname), 0777)
-
-		zreader, err := f.Open()
-		if err != nil {
-			log.Println(oname, err)
-		}
-		ofile, err := os.Create(oname)
-		if err != nil {
-			log.Println(oname, err)
-		}
-		_, err = io.Copy(ofile, zreader)
-		if err != nil {
-			log.Println(oname, err)
-		}
-
-		ofile.Close()
-		zreader.Close()
-	}
-
-	return nil
+	return archive.Extract(base, file, skip_dirs)
 }
 
 // download 'url' and try to parse the zip-file. print out
@@ -79,20 +46,21 @@ func acquire(base, url string, skip_dirs int, checkSha1 string) error {
 // TODO: dry_acquire is not nice with ui 'oneline' (or future 'curses' based
 // ones)
 //
-func dry_acquire(base, url string, skip_dirs int, checkSha1 string) error {
+func dry_acquire(base, url string, archive PluginArchive, skip_dirs int, checkSha1 string) ([]string, error) {
 
 	var (
-		err    error
-		resp   *http.Response
-		buffer *bytes.Buffer
-		zfile  *zip.Reader
+		err     error
+		resp    *http.Response
+		buffer  *bytes.Buffer
+		br      *bytes.Reader
+		entries []string
 	)
 
 	if resp, err = http.Get(url); err != nil {
-		return err
+		return nil, err
 	} else if resp.StatusCode != 200 {
 		log.Println(resp)
-		return fmt.Errorf("%d for %q", resp.StatusCode, url)
+		return nil, fmt.Errorf("%d for %q", resp.StatusCode, url)
 	}
 	defer resp.Body.Close()
 
@@ -101,36 +69,24 @@ func dry_acquire(base, url string, skip_dirs int, checkSha1 string) error {
 
 	buffer = bytes.NewBuffer(nil)
 	if _, err = io.Copy(buffer, tee); err != nil {
-		return err
+		return nil, err
 	}
 	defer buffer.Reset()
 
 	sha1Sum := hex.EncodeToString(hasher.Sum(nil))
 
 	if checkSha1 != "" && checkSha1 != sha1Sum {
-		return fmt.Errorf("sha1 does not match: got %s, expected %s", sha1Sum, checkSha1)
+		return nil, fmt.Errorf("sha1 does not match: got %s, expected %s", sha1Sum, checkSha1)
+	}
+	br = bytes.NewReader(buffer.Bytes())
+	entries, err = archive.Entries(br, skip_dirs)
+
+	if err != nil {
+		return nil, fmt.Errorf("gettting contents: %v", err)
+	}
+	if len(entries) == 0 {
+		return nil, fmt.Errorf("empty archive for %q", url)
 	}
 
-	if zfile, err = zip.NewReader(bytes.NewReader(buffer.Bytes()), int64(buffer.Len())); err != nil {
-		return err
-	}
-
-	for _, f := range zfile.File {
-
-		idx := index_byte_n(f.Name, '/', skip_dirs)
-		oname := f.Name[idx+1:]
-
-		// root-directory
-		//   pname/      <- root-directory
-		//   pname/a.vim
-		if oname == "" {
-			continue
-		}
-
-		oname = filepath.Join(base, filepath.Clean(oname))
-
-		fmt.Printf("%s\t%d\n", oname, f.FileHeader.UncompressedSize)
-	}
-
-	return nil
+	return entries, nil
 }

act_update.go
@@ -21,72 +21,98 @@ func act_update(plugins PluginList, ui JobUi, opts *actUpdateOptions) {
 
 	ui.Start()
 
+	var err error
+
 	for _, plugin := range plugins {
 
 		plugin_folder := filepath.Join(opts.dir, plugin.name)
 
-		if _, err := os.Stat(plugin_folder); err == nil { // plugin_folder exists
+		if _, err = os.Stat(plugin_folder); err == nil { // plugin_folder exists
 			if !opts.force {
 				continue
 			}
 		}
 
-		if !strings.HasSuffix(plugin.url.Path, ".zip") {
+		archive_name := filepath.Base(plugin.url.Path)
+		plugin.ext = filepath.Ext(archive_name)
+
+		// apply heuristics aka ""guess""
+		if plugin.ext == "" {
 			switch plugin.url.Host {
 			case "github.com":
 				remote_zip := first_not_empty(plugin.url.Fragment, "master") + ".zip"
 				plugin.url.Path = path.Join(plugin.url.Path, "archive", remote_zip)
+				archive_name = filepath.Base(remote_zip)
+				plugin.ext = ".zip"
+				plugin.archive = &ZipArchive{}
 			default:
-				ext, err := httpdetect_ftype(plugin.url.String())
+				plugin.ext, err = httpdetect_ftype(plugin.url.String())
 				if err != nil {
 					log.Printf("error: %q: %s", plugin.url, err)
 					continue
 				}
-				if ext != ".zip" {
-					log.Printf("error: %q: not a zip", plugin.url)
-					continue
-				}
+				archive_name += plugin.ext
+			}
+		}
+
+		if ok, suffix_len := IsSupportedArchive(archive_name); ok {
+			plugin.ext = archive_name[len(archive_name)-suffix_len:]
+		}
+
+		if plugin.archive == nil {
+			plugin.archive, err = GuessPluginArchive(archive_name)
+			if err != nil {
+				log.Printf("error: %q: not supported archive format", plugin.url)
+				continue
 			}
 		}
 
 		ui.AddJob(plugin_folder)
-		go acquire_and_postupdate(plugin_folder, plugin.sha1, opts.dry_run, plugin, ui)
+		go acquire_and_postupdate(plugin_folder, opts.dry_run, plugin, ui)
 	}
 
 	ui.Wait()
 	ui.Stop()
 }
 
-func acquire_and_postupdate(dir, sha1 string, dry_run bool, plugin *Plugin, ui JobUi) {
+func acquire_and_postupdate(dir string, dry_run bool, plugin *Plugin, ui JobUi) {
 
 	defer ui.JobDone(dir)
 
 	var (
-		err  error
-		path string
-		out  []byte
+		err     error
+		path    string
+		out     []byte
+		entries []string
 
-		acquire_f func(string, string, int, string) error = acquire
+		url       = plugin.url.String()
+		strip_dir = plugin.opts.strip_dir
+		sha1      = plugin.opts.sha1
 	)
 
 	if dry_run {
-		acquire_f = dry_acquire
+		entries, err = dry_acquire(dir, url, plugin.archive, strip_dir, sha1)
+	} else {
+		err = acquire(dir, plugin.ext, url, plugin.archive, strip_dir, sha1)
 	}
-	if err = acquire_f(dir, plugin.url.String(), plugin.strip_dir, sha1); err != nil {
+	if err != nil {
 		log.Printf("%s: %v", dir, err)
 		return
+	}
+	if dry_run {
+		ui.Print(dir, strings.Join(entries, "\n"))
 	}
 
 	//
 	// handle the .postupdate hook
 	//
-	if plugin.postupdate == "" {
+	if plugin.opts.postupdate == "" {
 		return
 	}
 
-	path, err = expand_path(plugin.postupdate)
+	path, err = expand_path(plugin.opts.postupdate)
 	if err != nil {
-		log.Printf("%s: expanding .postupdate %q: %s", dir, plugin.postupdate, err)
+		log.Printf("%s: expanding .postupdate %q: %s", dir, plugin.opts.postupdate, err)
 		return
 	}
 	path = expand_path_environment(path, dir)
@@ -103,12 +129,13 @@ func acquire_and_postupdate(dir, sha1 string, dry_run bool, plugin *Plugin, ui J
 		Path: path,
 		Env: append(os.Environ(),
 			"VOPHER_NAME="+plugin.name,
+			"VOPHER_ARCHIVE="+plugin.name+plugin.ext,
 			"VOPHER_DIR="+dir,
-			"VOPHER_URL="+plugin.url.String()),
+			"VOPHER_URL="+url),
 	}
 
 	if dry_run {
-		fmt.Printf("# postupdate: %q (env: %v)\n", cmd.Path, cmd.Env)
+		ui.Print(dir, fmt.Sprintf("# postupdate: %q (env: %v)\n", cmd.Path, cmd.Env))
 		return
 	}
 

archive.go
@@ -1,0 +1,120 @@
+package main
+
+import (
+	"compress/bzip2"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"path/filepath"
+	"strings"
+
+	"code.google.com/p/lzma"
+)
+
+var supported_archives = [...]string{
+	".zip",
+	".vba", ".vba.gz",
+	".tar.gz",
+	".tar.lzma",
+	".tar.bz2", ".tar.bzip2",
+	".tar.xz",
+}
+
+// returns true/false if "name" is a supported archive type
+// and the length of the suffix. eg, ".zip" yields 4, ".vba.gz"
+// yields 7.
+func IsSupportedArchive(name string) (bool, int) {
+	name = strings.ToLower(name)
+	for i := range supported_archives {
+		if strings.HasSuffix(name, supported_archives[i]) {
+			return true, len(supported_archives[i])
+		}
+	}
+	return false, 0
+}
+
+// a PluginArchive is the interface to show and extract entries
+// from an archived version of a given plugin. this means zipped
+// or tar.gzipped or vimballs
+type PluginArchive interface {
+	Extract(folder string, r io.Reader, skip_dirs int) error
+	Entries(r io.Reader, skip_dirs int) ([]string, error)
+}
+
+func GuessPluginArchive(name string) (PluginArchive, error) {
+	n := strings.ToLower(name)
+	if strings.HasSuffix(n, ".zip") {
+		return &ZipArchive{}, nil
+	} else if strings.HasSuffix(n, ".vba") {
+		return &VimballArchive{}, nil
+	} else if strings.HasSuffix(n, ".vba.gz") {
+		return &GzArchive{&VimballArchive{}}, nil
+	} else if strings.HasSuffix(n, ".tar.gz") {
+		return &GzArchive{&TarArchive{}}, nil
+	} else if strings.HasSuffix(n, ".tar.bz2") || strings.HasSuffix(n, ".tar.bzip2") {
+		return &BzipArchive{&TarArchive{}}, nil
+	} else if strings.HasSuffix(n, ".tar.xz") {
+		return &LzmaArchive{&TarArchive{}}, nil
+	}
+	return nil, fmt.Errorf("unsupported archive type for %q\n", name)
+}
+
+// wrapper to decompress gzip
+type GzArchive struct{ orig PluginArchive }
+
+func (ga *GzArchive) Extract(folder string, r io.Reader, skip_dir int) error {
+	gzreader, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	return ga.orig.Extract(folder, gzreader, skip_dir)
+}
+
+func (ga *GzArchive) Entries(r io.Reader, skip_dir int) ([]string, error) {
+	gzreader, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+	return ga.orig.Entries(gzreader, skip_dir)
+}
+
+// wrapper to decompress bzip2
+type BzipArchive struct{ orig PluginArchive }
+
+func (ba *BzipArchive) Extract(folder string, r io.Reader, skip_dir int) error {
+	bzreader := bzip2.NewReader(r)
+	return ba.orig.Extract(folder, bzreader, skip_dir)
+}
+
+func (ba *BzipArchive) Entries(r io.Reader, skip_dir int) ([]string, error) {
+	bzreader := bzip2.NewReader(r)
+	return ba.orig.Entries(bzreader, skip_dir)
+}
+
+// wrapper to decompress lzma
+type LzmaArchive struct{ orig PluginArchive }
+
+func (la *LzmaArchive) Extract(folder string, r io.Reader, skip_dir int) error {
+	lzma_reader := lzma.NewReader(r)
+	defer lzma_reader.Close()
+	return la.orig.Extract(folder, lzma_reader, skip_dir)
+}
+
+func (la *LzmaArchive) Entries(r io.Reader, skip_dir int) ([]string, error) {
+	lzma_reader := lzma.NewReader(r)
+	defer lzma_reader.Close()
+	return la.Entries(lzma_reader, skip_dir)
+}
+
+// strip away the leading 'strip_dirs' directories from 'name'. returns
+// the stripped named AND a bool indicating, if the entry should be skipped
+// because it's the root-direktory
+//
+//      name/      <- root-directory, will be stripped
+//      name/a.vim
+func StripArchiveEntry(name string, strip_dirs int) (stripped_name string, is_root bool) {
+	name = filepath.ToSlash(name)
+	idx := index_byte_n(name, '/', strip_dirs)
+	name = name[idx+1:]
+	return name, (name == "")
+}

archive_tar.go
@@ -1,0 +1,83 @@
+package main
+
+import (
+	"archive/tar"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+type TarArchive struct{}
+
+func (ta *TarArchive) Extract(folder string, r io.Reader, strip_dirs int) error {
+	_, err := ta.handle(folder, r, strip_dirs, tar_extract_entry)
+	return err
+}
+
+func (ta *TarArchive) Entries(r io.Reader, strip_dirs int) ([]string, error) {
+	return ta.handle("", r, strip_dirs, tar_ignore_entry)
+}
+
+// small helper to operate on a tar-entry. reader r points directly
+// to the data for 'name' in the tar file.
+type tar_efunc func(name string, r io.Reader) error
+
+// handle all file-like entries in the tar represented by 'r' due the 'extract'
+// function.
+// TODO: make sure "file-like" is the correct criteria.
+func (ta *TarArchive) handle(folder string, r io.Reader, strip_dirs int, extract tar_efunc) ([]string, error) {
+
+	var (
+		entries = make([]string, 0)
+		reader  = tar.NewReader(r)
+	)
+
+	for {
+		header, err := reader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		if header.Typeflag != tar.TypeReg && header.Typeflag != tar.TypeRegA {
+			continue
+		}
+
+		fi := header.FileInfo()
+		if fi.IsDir() {
+			// TODO: decide, if we skip dirs or not
+			continue
+		} else if header.Name != "" && header.Name[0] == '/' {
+			return nil, fmt.Errorf("entry with absolute filename %q", header.Name)
+		}
+
+		oname, is_root := StripArchiveEntry(header.Name, strip_dirs)
+		if is_root {
+			continue
+		}
+		entries = append(entries, oname)
+		if err := extract(filepath.Join(folder, oname), reader); err != nil {
+			return nil, err
+		}
+	}
+	return entries, nil
+}
+
+func tar_extract_entry(name string, r io.Reader) error {
+	if err := os.MkdirAll(filepath.Dir(name), 0777); err != nil {
+		return err
+	}
+	file, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(file, r)
+	return err
+}
+func tar_ignore_entry(name string, r io.Reader) error {
+	_, err := io.Copy(ioutil.Discard, r)
+	return err
+}

archive_vimball.go
@@ -1,0 +1,136 @@
+package main
+
+// http://www.drchip.org/astronaut/vim/doc/pi_vimball.txt.html
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type vimball_efunc func(name string, lines int, s *bufio.Scanner) error
+
+type VimballArchive struct{}
+
+func (vimball *VimballArchive) Extract(folder string, r io.Reader, skip_dir int) error {
+
+	f := func(n string, l int, s *bufio.Scanner) error {
+		return vimball.extractFile(n, l, s)
+	}
+	_, err := vimball.handle(folder, r, f)
+	return err
+}
+
+func (vimball *VimballArchive) Entries(r io.Reader, skip_dir int) ([]string, error) {
+	f := func(n string, l int, s *bufio.Scanner) error {
+		return vimball.skipFile(n, l, s)
+	}
+	return vimball.handle("", r, f)
+}
+
+// extracts the contents a vimball formatted 'r' into 'dir'
+// the format works like this:
+//
+// preamble
+//     " Vimball Archive by Charles E. Campbell, Jr. Ph.D.
+//     UseVimball
+//     finish
+// file-contents
+//     folder/name_of_file
+//     number_of_lines
+//     ...
+//     ...
+//     folder2/other_file
+//     number_of_lines2
+//     ...
+//     ...
+func (*VimballArchive) handle(folder string, r io.Reader, extract vimball_efunc) ([]string, error) {
+
+	var (
+		contents = make([]string, 0)
+		scanner  = bufio.NewScanner(r)
+
+		pre = struct{ use_vimball, finish bool }{} // 'preamble'
+	)
+
+	scanner.Split(bufio.ScanLines)
+
+	// scan for lines 'UseVimball', followed by 'finish'
+	for scanner.Scan() && !pre.use_vimball && !pre.finish {
+		line := scanner.Text()
+		if !pre.use_vimball && line == "UseVimball" {
+			pre.use_vimball = true
+		} else if pre.use_vimball && !pre.finish && line == "finish" {
+			pre.finish = true
+		}
+	}
+
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	if !pre.use_vimball && !pre.finish {
+		return nil, fmt.Errorf("error vimball: strange preamble")
+	}
+
+	// now scan the file-entries
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.SplitN(line, "\t", 2)
+
+		name := strings.TrimSpace(fields[0])
+		name = filepath.Clean(name)
+
+		if !scanner.Scan() {
+			return nil, fmt.Errorf("error vimball: while scanning line-number for %q: %v", name, scanner.Err())
+		}
+
+		nlines, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, fmt.Errorf("error vimball: while parsing line-number for %q: %v", name, err)
+		}
+		if nlines < 0 {
+			return nil, fmt.Errorf("error vimball: got negative line-number for %q: %v", name, nlines)
+		}
+
+		if err = extract(filepath.Join(folder, name), nlines, scanner); err != nil {
+			return nil, err
+		}
+		contents = append(contents, name)
+	}
+
+	return contents, scanner.Err()
+}
+
+//
+func (*VimballArchive) extractFile(name string, lines int, scanner *bufio.Scanner) error {
+
+	dir := filepath.Dir(name)
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return err
+	}
+
+	file, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for lines > 0 && scanner.Scan() {
+		file.Write(scanner.Bytes())
+		lines--
+	}
+
+	return scanner.Err()
+}
+
+func (*VimballArchive) skipFile(name string, lines int, scanner *bufio.Scanner) error {
+	for lines > 0 && scanner.Scan() {
+		lines--
+	}
+	return scanner.Err()
+}

archive_zip.go
@@ -1,0 +1,95 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type ZipArchive struct{}
+
+func (za *ZipArchive) Extract(folder string, r io.Reader, strip_dirs int) error {
+
+	zfile, err := zip_openReader(r)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range zfile.File {
+
+		oname, is_root := StripArchiveEntry(f.Name, strip_dirs)
+		if is_root {
+			continue
+		}
+
+		oname = filepath.Join(folder, filepath.Clean(oname))
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(oname, 0777)
+			continue
+		}
+
+		// TODO: call only if needed
+		os.MkdirAll(filepath.Dir(oname), 0777)
+
+		zreader, err := f.Open()
+		if err != nil {
+			log.Println(oname, err)
+		}
+		ofile, err := os.Create(oname)
+		if err != nil {
+			log.Println(oname, err)
+		}
+		_, err = io.Copy(ofile, zreader)
+		if err != nil {
+			log.Println(oname, err)
+		}
+
+		ofile.Close()
+		zreader.Close()
+	}
+
+	return nil
+}
+
+func (*ZipArchive) Entries(r io.Reader, strip_dirs int) ([]string, error) {
+
+	var (
+		entries    = make([]string, 0)
+		zfile, err = zip_openReader(r)
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range zfile.File {
+		oname, is_root := StripArchiveEntry(f.Name, strip_dirs)
+		if is_root {
+			continue
+		}
+		entries = append(entries, filepath.Clean(oname))
+	}
+
+	return entries, nil
+}
+
+func zip_openReader(r io.Reader) (*zip.Reader, error) {
+	switch rt := r.(type) {
+	default:
+		buffer := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buffer, r); err != nil {
+			return nil, err
+		}
+		br := bytes.NewReader(buffer.Bytes())
+		return zip.NewReader(br, int64(buffer.Len()))
+	case *os.File:
+		fi, err := rt.Stat()
+		if err != nil {
+			return nil, err
+		}
+		return zip.NewReader(rt, fi.Size())
+	}
+}

plugin.go
@@ -1,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	neturl "net/url"
+)
+
+const (
+
+	// most plugins are fetched from github. the github zip-files
+	// put the files into a subfolder like this:
+	//   vim-plugin/doc/plugin.txt
+	//   vim-plugin/README.txt
+	//
+	DEFAULT_STRIP = 1
+
+	OPT_STRIP_DIR     = 0
+	OPT_POSTUPDATE    = 1
+	OPT_POSTUPDATE_OS = 2
+	OPT_SHA1          = 3
+)
+
+type Plugin struct {
+	name string
+	ext  string
+	url  *neturl.URL
+	opts struct {
+		strip_dir  int    // strip n dir-parts from archive-entries
+		postupdate string // execute after 'update'-action
+		sha1       string
+	}
+	archive PluginArchive // used to extract/view content of plugin
+}
+
+func (pl *Plugin) String() string {
+	return fmt.Sprintf("Plugin{%q, %q, strip=%d}",
+		pl.name, pl.url.String(), pl.opts.strip_dir)
+}

plugin_file.go
@@ -18,19 +18,7 @@ import (
 )
 
 const (
-	// most plugins are fetched from github. the github zip-files
-	// put the files into a subfolder like this:
-	//   vim-plugin/doc/plugin.txt
-	//   vim-plugin/README.txt
-	//
-	DEFAULT_STRIP = 1
-
 	BYTE_ORDER_MARK = '\uFEFF'
-
-	OPT_STRIP_DIR     = 0
-	OPT_POSTUPDATE    = 1
-	OPT_POSTUPDATE_OS = 2
-	OPT_SHA1          = 3
 )
 
 var PLUGIN_OPTS = []string{
@@ -38,19 +26,6 @@ var PLUGIN_OPTS = []string{
 	"postupdate=",
 	"postupdate." + runtime.GOOS + "=",
 	"sha1=",
-}
-
-type Plugin struct {
-	name       string
-	url        *neturl.URL
-	strip_dir  int    // strip n dir-parts from archive-entries
-	postupdate string // execute after 'update'-action
-	sha1       string
-}
-
-func (pl *Plugin) String() string {
-	return fmt.Sprintf("Plugin{%q, %q, strip=%d}",
-		pl.name, pl.url.String(), pl.strip_dir)
 }
 
 type PluginList map[string]*Plugin
@@ -106,11 +81,12 @@ func ScanPluginReader(reader io.ReadCloser) (plugins PluginList, err error) {
 		}
 
 		// strip away .zip (or other archive-formats)
-		if strings.HasSuffix(name, ".zip") {
-			name = name[:len(name)-4]
+		if ok, len_suffix := IsSupportedArchive(name); ok {
+			name = name[:len(name)-len_suffix]
 		}
 
-		plugin := Plugin{name: name, url: url, strip_dir: DEFAULT_STRIP}
+		plugin := Plugin{name: name, url: url}
+		plugin.opts.strip_dir = DEFAULT_STRIP
 		if err = plugin.OptionsFromFields(fields[1:]); err != nil {
 			return nil, fmt.Errorf("parsing optional fields: %q, plugin %q on line %d", err, name, lnumber)
 		}
@@ -122,32 +98,32 @@ func ScanPluginReader(reader io.ReadCloser) (plugins PluginList, err error) {
 
 func (p *Plugin) OptionsFromFields(fields []string) error {
 
-	for i := range fields {
-		if strings.HasPrefix(fields[i], PLUGIN_OPTS[OPT_STRIP_DIR]) {
-			strip, err := strconv.ParseUint((fields[i])[len(PLUGIN_OPTS[OPT_STRIP_DIR]):], 10, 8)
+	for _, field := range fields {
+		if strings.HasPrefix(field, PLUGIN_OPTS[OPT_STRIP_DIR]) {
+			strip, err := strconv.ParseUint(field[len(PLUGIN_OPTS[OPT_STRIP_DIR]):], 10, 8)
 			if err == nil {
-				p.strip_dir = int(strip)
+				p.opts.strip_dir = int(strip)
 			} else {
 				return fmt.Errorf("strange 'strip' field")
 			}
-		} else if strings.HasPrefix(fields[i], PLUGIN_OPTS[OPT_POSTUPDATE]) && p.postupdate == "" {
-			p.postupdate = fields[i][len(PLUGIN_OPTS[OPT_POSTUPDATE]):]
-		} else if strings.HasPrefix(fields[i], PLUGIN_OPTS[OPT_POSTUPDATE_OS]) {
-			p.postupdate = fields[i][len(PLUGIN_OPTS[OPT_POSTUPDATE_OS]):]
-		} else if strings.HasPrefix(fields[i], PLUGIN_OPTS[OPT_SHA1]) {
-			p.sha1 = strings.ToLower(fields[i][len(PLUGIN_OPTS[OPT_SHA1]):])
+		} else if strings.HasPrefix(field, PLUGIN_OPTS[OPT_POSTUPDATE]) && p.opts.postupdate == "" {
+			p.opts.postupdate = field[len(PLUGIN_OPTS[OPT_POSTUPDATE]):]
+		} else if strings.HasPrefix(field, PLUGIN_OPTS[OPT_POSTUPDATE_OS]) {
+			p.opts.postupdate = field[len(PLUGIN_OPTS[OPT_POSTUPDATE_OS]):]
+		} else if strings.HasPrefix(field, PLUGIN_OPTS[OPT_SHA1]) {
+			p.opts.sha1 = strings.ToLower(field[len(PLUGIN_OPTS[OPT_SHA1]):])
 		}
 	}
 
-	if p.postupdate != "" {
-		decoded, err := neturl.QueryUnescape(p.postupdate)
+	if p.opts.postupdate != "" {
+		decoded, err := neturl.QueryUnescape(p.opts.postupdate)
 		if err != nil {
 			return err
 		}
-		p.postupdate = decoded
+		p.opts.postupdate = decoded
 	}
 
-	if p.sha1 != "" && len(p.sha1) != hex.EncodedLen(sha1.Size) {
+	if p.opts.sha1 != "" && len(p.opts.sha1) != hex.EncodedLen(sha1.Size) {
 		return fmt.Errorf("'sha1' field does not match size of a sha1")
 	}
 

commit 96bdbf82603ed3f5439f2ec8c3f1a5118a71aaa5

fix bug: recognize plugins like 'unite.vim' (again)
the bug was caused by small regression introduced by a4e7ef8.

acquire.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 // fetch 'url' and extract it into 'base'. skip 'skip_dirs'
@@ -17,16 +16,11 @@ import (
 // the contents.
 func acquire(base, ext, url string, archive PluginArchive, skip_dirs int, checkSha1 string) error {
 
-	var (
-		name = base
-		err  = os.MkdirAll(name, 0777)
-	)
+	name := base + ext
+	err := os.MkdirAll(base, 0777)
 
 	if err != nil {
 		return fmt.Errorf("mkdir %q: %s", base, err)
-	}
-	if filepath.Ext(name) == "" {
-		name += ext
 	}
 	if err = httpget(name, url, checkSha1); err != nil {
 		return err

act_update.go
@@ -34,10 +34,9 @@ func act_update(plugins PluginList, ui JobUi, opts *actUpdateOptions) {
 		}
 
 		archive_name := filepath.Base(plugin.url.Path)
-		plugin.ext = filepath.Ext(archive_name)
 
 		// apply heuristics aka ""guess""
-		if plugin.ext == "" {
+		if is_archive, _ := IsSupportedArchive(archive_name); !is_archive {
 			switch plugin.url.Host {
 			case "github.com":
 				remote_zip := first_not_empty(plugin.url.Fragment, "master") + ".zip"