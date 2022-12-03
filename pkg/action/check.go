4989f6498009db53e3a1f48b77668915a0c201ea
Mathias Gumz <mg@2hoch5.com>
2022-12-03 01:05:48 +0100

    Feature: improve output of "check"
    This commit aligns the plugin names when printing the result of
    the "check" action: On the left side the name of each plugin is
    vertically aligned with all other plugin names which makes the
    actual change output much more stable and better readable.

--- pkg/action/check.go
+++ pkg/action/check.go
@@ -1,6 +1,8 @@
 package action
 
 import (
+	"fmt"
+	"strconv"
 	"sync"
 
 	"github.com/mgumz/vopher/pkg/acquire"
@@ -30,12 +32,27 @@ func Check(plugins plugin.List, base string, ui ui.UI) {
 		}
 	}
 
+	idFmt := calcIdFmt(plugins)
 	wg.Wait()
 
 	for _, id := range plugins.SortByLineNumber() {
 		if t, ok := m.Load(id); ok {
 			text, _ := t.(string)
+			// right aligned Id
+			id := fmt.Sprintf(idFmt, id)
 			ui.Print(id, text)
 		}
 	}
+}
+
+func calcIdFmt(plugins plugin.List) string {
+
+	w := 0
+	for _, p := range plugins {
+		if len(p.Name) > w {
+			w = len(p.Name)
+		}
+	}
+	return "%" + strconv.Itoa(w) + "s"
+
 }
